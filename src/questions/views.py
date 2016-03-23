0bb084ce1c39f3852d8dbfbdf82b8c7af8dc592f
Schweigs <Gith-123>
2016-03-23 19:09:31 +0000

    Lecture 11: Question Form

--- src/questions/views.py
+++ src/questions/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, Http404
+from .models import Question, Answer
 
-# Create your views here.
+from .forms import UserResponseForm
+
+
+def home(request):
+    if request.user.is_authenticated():
+        form = UserResponseForm(request.POST or None)
+        if form.is_valid():
+            print form.cleaned_data
+            question_id = form.cleaned_data.get('question_id')
+            answer_id = form.cleaned_data.get('answer_id')
+            question_instance = Question.objects.get(id=question_id)
+            answer_instance = Answer.objects.get(id=answer_id)
+            print question_instance.text, answer_instance.text
+
+        queryset = Question.objects.all().order_by('-timestamp')
+        instance = queryset[0]
+        context = {
+            "form": form,
+            "instance": instance,
+            # "queryset": queryset
+        }
+        return render(request, "questions/home.html", context)
+    else:
+        raise Http404
