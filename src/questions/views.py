3109eca225af34185fe4297f7a3ce97a31097819
Schweigs <Gith-123>
2016-03-27 16:15:30 +0100

    Lecture 12: Question Single 5 mins into video

--- src/questions/views.py
+++ src/questions/views.py
@@ -1,7 +1,35 @@
-from django.shortcuts import render, Http404
+from django.http import Http404
+from django.shortcuts import render
 from .models import Question, Answer
 
 from .forms import UserResponseForm
+
+
+def single(request, id):
+    try:
+        instance = Question.objects.get(id=id)
+    except:
+        raise Http404
+    if request.user.is_authenticated():
+        form = UserResponseForm(request.POST or None)
+        if form.is_valid():
+            print form.cleaned_data
+            question_id = form.cleaned_data.get('question_id')
+            answer_id = form.cleaned_data.get('answer_id')
+            question_instance = Question.objects.get(id=question_id)
+            answer_instance = Answer.objects.get(id=answer_id)
+            print question_instance.text, answer_instance.text
+
+        queryset = Question.objects.all().order_by('-timestamp')
+        instance = queryset[id]
+        context = {
+            "form": form,
+            "instance": instance,
+            # "queryset": queryset
+        }
+        return render(request, "questions/home.html", context)
+    else:
+        raise Http404
 
 
 def home(request):
